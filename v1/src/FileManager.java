515ef9b84302cdac8e1fc9d8b777f960a4c557b0
transmigrator <project@transmigrator.org>
2024-10-10 14:39:31 +0200

    FileManager.java

--- v1/src/FileManager.java
+++ v1/src/FileManager.java
@@ -1,16 +1,20 @@
 import javax.microedition.io.Connector;
 import javax.microedition.io.FileConnection;
-import javax.microedition.io.HttpsConnection;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.File;
+import java.io.IOException;
 
 public class FileManager {
     private String downloadDir;
-    private String uploadDir;
 
     public FileManager() {
-        this.downloadDir = System.getProperty("user.home");
-        this.uploadDir = System.getProperty("user.home");
+        if (isJ2ME()) {
+            downloadDir = "file:///root1/";
+        } else {
+            downloadDir = System.getProperty("java.io.tmpdir");
+            if (downloadDir == null) {
+                downloadDir = System.getProperty("user.home");
+            }
+        }
     }
 
     public String getDownloadDir() {
@@ -21,111 +25,48 @@ public class FileManager {
         this.downloadDir = downloadDir;
     }
 
-    public String getUploadDir() {
-        return uploadDir;
+    public String selectFileForUpload() {
+        return FileChooser.selectFile();
     }
 
-    public void setUploadDir(String uploadDir) {
-        this.uploadDir = uploadDir;
+    public void downloadFile(String fileName, String fileContent) {
+        if (isJ2ME()) {
+            downloadFileJ2ME(fileName, fileContent);
+        } else {
+            downloadFileJVM(fileName, fileContent);
+        }
     }
 
-    public String selectFile() {
-        return FileUtils.selectFile();
+    private boolean isJ2ME() {
+        return System.getProperty("microedition.configuration") != null;
     }
 
-    public void downloadFile(String url, String filename) {
+    private void downloadFileJ2ME(String fileName, String fileContent) {
         try {
-            // Download the file from the given URL
-            HttpsConnection hc = (HttpsConnection) Connector.open(url);
-            InputStream in = hc.openInputStream();
-            // Save the file to the download directory
-            FileConnection fc = (FileConnection) Connector.open("file://" + downloadDir + "/" + filename);
-            OutputStream out = fc.openOutputStream();
-            byte[] buffer = new byte[1024];
-            int len;
-            while ((len = in.read(buffer)) != -1) {
-                out.write(buffer, 0, len);
+            FileConnection fc = (FileConnection) Connector.open(downloadDir + fileName);
+            if (!fc.exists()) {
+                fc.create();
             }
-            in.close();
-            out.close();
+            OutputStream os = fc.openOutputStream();
+            os.write(fileContent.getBytes());
+            os.close();
             fc.close();
         } catch (Exception e) {
             // Handle exception
         }
     }
 
-    public void uploadFile(String url) {
-        // Select a file from the local file system
-        String selectedFile = FileUtils.selectFile();
-
-        // If a file was selected, upload it to the specified URL
-        if (selectedFile != null) {
-            try {
-                // Create a connection to the URL
-                HttpsConnection hc = (HttpsConnection) Connector.open(url);
-
-                // Set the request method to POST
-                hc.setRequestMethod(HttpsConnection.POST);
-
-                // Set the content type to multipart/form-data
-                hc.setRequestProperty("Content-Type", "multipart/form-data; boundary=---------------------------boundary");
-
-                // Create an output stream to write the file data to
-                OutputStream os = hc.openOutputStream();
-
-                // Write the file data to the output stream
-                byte[] fileData = getFileData(selectedFile);
-                os.write(fileData);
-
-                // Close the output stream
-                os.close();
-
-                // Get the response code from the server
-                int responseCode = hc.getResponseCode();
-
-                // If the response code is 200, the file was uploaded successfully
-                if (responseCode == HttpsConnection.HTTP_OK) {
-                    System.out.println("File uploaded successfully!");
-                } else {
-                    System.out.println("Error uploading file: " + responseCode);
-                }
-
-                // Close the connection
-                hc.close();
-            } catch (Exception e) {
-                System.out.println("Error uploading file: " + e.getMessage());
-            }
-        }
-    }
-
-    private byte[] getFileData(String filePath) {
+    private void downloadFileJVM(String fileName, String fileContent) {
         try {
-            // Create a file connection to the selected file
-            FileConnection fc = (FileConnection) Connector.open("file://" + filePath);
-
-            // Get the file size
-            int fileSize = (int) fc.fileSize();
-
-            // Create a byte array to store the file data
-            byte[] fileData = new byte[fileSize];
-
-            // Create an input stream to read the file data
-            InputStream is = fc.openInputStream();
-
-            // Read the file data into the byte array
-            is.read(fileData);
-
-            // Close the input stream
-            is.close();
-
-            // Close the file connection
-            fc.close();
-
-            // Return the file data
-            return fileData;
-        } catch (Exception e) {
-            System.out.println("Error reading file data: " + e.getMessage());
-            return null;
+            File file = new File(downloadDir + fileName);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileWriter fw = new FileWriter(file);
+            fw.write(fileContent);
+            fw.close();
+        } catch (IOException e) {
+            // Handle exception
         }
     }
 }
