9ddcdc952e9887a6b82e807b87548bc2f8df0ad1
transmigrator <project@transmigrator.org>
2024-10-10 22:30:31 +0200

    SshuttleGateway.java

--- v1/src/SshuttleGateway.java
+++ v1/src/SshuttleGateway.java
@@ -1,70 +1,54 @@
-import jpype.PythonObject;
-import jpype.PythonInterpreter;
-import java.security.SecureRandom;
+import org.xbill.DNS.*;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 
 public class SshuttleGateway {
-    public SshuttleGateway() {
-        // Start the Python interpreter
-        PythonInterpreter python = new PythonInterpreter();
-        python.exec("import sshuttle");
+    private String dohProxyServer;
+    private int dohProxyPort;
+    private String sshuttleProxyServer;
+    private int sshuttleProxyPort;
 
-        // Get the sshuttle object
-        PythonObject sshuttle = python.get("sshuttle");
-
-        // Generate or retrieve the data to be encrypted
-        String dataToEncrypt = generateSensitiveData();
-
-        try {
-            // Generate a keypair
-            PythonObject keypair = sshuttle.invoke("generate_keypair");
-            PythonObject private_key = keypair.__getitem__(0);
-            PythonObject public_key = keypair.__getitem__(1);
-
-            // Perform ECDH key exchange
-            PythonObject peer_public_key = sshuttle.invoke("generate_keypair").__getitem__(1);
-            PythonObject shared_secret = sshuttle.invoke("ecdh_key_exchange", private_key, peer_public_key);
-
-            // Derive a symmetric key
-            PythonObject symmetric_key = sshuttle.invoke("derive_symmetric_key", shared_secret);
-
-            // Encrypt the data
-            PythonObject encrypted_data = sshuttle.invoke("encrypt", dataToEncrypt.getBytes(), symmetric_key);
-
-            // Sign the data
-            PythonObject signature = sshuttle.invoke("sign", dataToEncrypt.getBytes(), private_key);
-
-            // Establish a TLS/SSL tunnel
-            String host = "localhost";
-            int port = 8080;
-            PythonObject response = sshuttle.invoke("connect", host, port);
-
-            System.out.println("Encrypted data: " + encrypted_data);
-            System.out.println("Signature: " + signature);
-            System.out.println("Server response: " + response);
-        } catch (Exception e) {
-            System.err.println("Error: " + e.getMessage());
-        }
+    public SshuttleGateway(String dohProxyServer, int dohProxyPort, String sshuttleProxyServer, int sshuttleProxyPort) {
+        this.dohProxyServer = dohProxyServer;
+        this.dohProxyPort = dohProxyPort;
+        this.sshuttleProxyServer = sshuttleProxyServer;
+        this.sshuttleProxyPort = sshuttleProxyPort;
     }
 
-    // Method to generate or retrieve the sensitive data
-    private String generateSensitiveData() {
-        // Use a secure random number generator
-        SecureRandom secureRandom = new SecureRandom();
-        byte[] sensitiveData = new byte[32];
-        secureRandom.nextBytes(sensitiveData);
-        return bytesToHex(sensitiveData);
+    public void setupDnsResolver() {
+        // Create a DoH resolver that uses the same proxy servers as sshuttle
+        ExtendedResolver resolver = new ExtendedResolver();
+        resolver.setHTTPS(true); // Use HTTPS for DNS queries
+        resolver.setPort(dohProxyPort); // Use port 443 for DoH
+        resolver.addServer(dohProxyServer); // Add the DoH proxy server
+
+        // Set the DoH resolver as the default DNS resolver
+        Lookup.setDefaultResolver(resolver);
     }
 
-    // Helper method to convert bytes to hex
-    private String bytesToHex(byte[] bytes) {
-        StringBuilder hexString = new StringBuilder();
-        for (byte b : bytes) {
-            String hex = Integer.toHexString(0xff & b);
-            if (hex.length() == 1) {
-                hexString.append('0');
-            }
-            hexString.append(hex);
-        }
-        return hexString.toString();
+    public Socket connect(String host, int port) throws IOException {
+        // Create a socket that uses the sshuttle proxy server
+        SocketAddress proxyAddress = new InetSocketAddress(sshuttleProxyServer, sshuttleProxyPort);
+        Proxy proxy = new Proxy(Proxy.Type.SOCKS, proxyAddress);
+        Socket socket = new Socket(proxy);
+
+        // Connect to the target host and port through the sshuttle proxy server
+        socket.connect(new InetSocketAddress(host, port));
+
+        // Upgrade the socket to an SSL socket
+        SSLSocketFactory sslSocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+        SSLSocket sslSocket = (SSLSocket) sslSocketFactory.createSocket(socket, host, port, true);
+
+        // Start the SSL handshake
+        sslSocket.startHandshake();
+
+        return sslSocket;
     }
 }
