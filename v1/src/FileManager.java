bcbeb6b192493a6fc9187c4e763a93e1821db1c3
transmigrator <project@transmigrator.org>
2024-10-10 13:47:27 +0200

    FileManager.java

--- v1/src/FileManager.java
+++ v1/src/FileManager.java
@@ -1,36 +1,63 @@
-// FileManager.java
-
-import java.io.File;
-import java.io.IOException;
+import javax.microedition.io.Connector;
+import javax.microedition.io.FileConnection;
+import javax.microedition.io.HttpConnection;
+import javax.microedition.io.HttpsConnection;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class FileManager {
-    private FileUtils fileUtils;
+    private String downloadDir;
+    private String uploadDir;
 
     public FileManager() {
-        fileUtils = new FileUtils();
+        this.downloadDir = System.getProperty("user.home");
+        this.uploadDir = System.getProperty("user.home");
     }
 
-    public void downloadFile(String url, String filename, FileProgressListener listener) {
-        fileUtils.downloadFile(url, filename, listener);
+    public String getDownloadDir() {
+        return downloadDir;
     }
 
-    public void uploadFile(String filename, String url) {
-        fileUtils.uploadFile(filename, url);
+    public void setDownloadDir(String downloadDir) {
+        this.downloadDir = downloadDir;
     }
 
-    public List<String> listFiles() {
-        return fileUtils.listFiles();
+    public String getUploadDir() {
+        return uploadDir;
     }
 
-    public void deleteFile(String filename) {
-        fileUtils.deleteFile(filename);
-    }
-
-    public void createDirectory(String directoryName) {
-        fileUtils.createDirectory(directoryName);
+    public void setUploadDir(String uploadDir) {
+        this.uploadDir = uploadDir;
     }
 
     public String selectFile() {
-        return fileUtils.selectFile();
+        return FileUtils.selectFile();
+    }
+
+    public void downloadFile(String url, String filename) {
+        try {
+            // Download the file from the given URL
+            HttpsConnection hc = (HttpsConnection) Connector.open(url);
+            InputStream in = hc.openInputStream();
+            // Save the file to the download directory
+            FileConnection fc = (FileConnection) Connector.open("file://" + downloadDir + "/" + filename);
+            OutputStream out = fc.openOutputStream();
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = in.read(buffer)) != -1) {
+                out.write(buffer, 0, len);
+            }
+            in.close();
+            out.close();
+            fc.close();
+        } catch (Exception e) {
+            // Handle exception
+        }
+    }
+
+    public void uploadFile(String filename) {
+        // This method is not implemented as it's not clear what the upload URL should be
+        // If you want to implement file upload, you should replace the URL with your server's upload URL
+        // and handle the upload logic accordingly
     }
 }
