7ffcc591fa333c15079dc6abf1604b35ab126ba5
transmigrator <project@transmigrator.org>
2024-10-10 15:53:25 +0200

    FileManager.java

--- v1/src/FileManager.java
+++ v1/src/FileManager.java
@@ -1,5 +1,3 @@
-// FileManager.java
-
 import javax.microedition.io.Connector;
 import javax.microedition.io.FileConnection;
 import java.io.File;
@@ -26,47 +24,22 @@ public class FileManager {
         }
     }
 
-    public String selectFileForUpload() {
-        if (isJ2ME()) {
-            return selectFileForUploadJ2ME();
-        } else {
-            return selectFileForUploadJVM();
-        }
-    }
-
-    private String selectFileForUploadJ2ME() {
-        try {
-            FileConnection fc = (FileConnection) Connector.open("file:///root1/");
-            if (fc.isDirectory()) {
-                Enumeration fileEnum = fc.list();
-                Vector fileVector = new Vector();
-                while (fileEnum.hasMoreElements()) {
-                    String file = (String) fileEnum.nextElement();
-                    fileVector.addElement(file);
-                }
-                // Allow the user to select a file from the list
-                // For simplicity, return the first file for now
-                String selectedFile = (String) fileVector.elementAt(0);
-                fc.close();
-                return selectedFile;
-            } else {
-                fc.close();
-                return null;
+    public void setDownloadDir(String downloadDir) {
+        this.downloadDir = downloadDir;
+        if (!isJ2ME()) {
+            File dir = new File(downloadDir);
+            if (!dir.exists()) {
+                dir.mkdirs();
             }
-        } catch (Exception e) {
-            return null;
         }
     }
 
-    private String selectFileForUploadJVM() {
-        JFileChooser fileChooser = new JFileChooser();
-        int returnValue = fileChooser.showOpenDialog(null);
-        if (returnValue == JFileChooser.APPROVE_OPTION) {
-            File selectedFile = fileChooser.getSelectedFile();
-            return selectedFile.getAbsolutePath();
-        } else {
-            return null;
+    public String getDownloadDir() {
+        if (downloadDir == null) {
+            // Return a default directory or handle this situation in a more user-friendly way
+            return System.getProperty("java.io.tmpdir");
         }
+        return downloadDir;
     }
 
     public void downloadFile(String fileName, String fileContent) {
@@ -114,21 +87,53 @@ public class FileManager {
         }
     }
 
-    public void setDownloadDir(String downloadDir) {
-        this.downloadDir = downloadDir;
-        if (!isJ2ME()) {
-            File dir = new File(downloadDir);
-            if (!dir.exists()) {
-                dir.mkdirs();
-            }
+    public String selectFileForUpload() {
+        if (isJ2ME()) {
+            return selectFileForUploadJ2ME();
+        } else {
+            return selectFileForUploadJVM();
         }
     }
 
-    public String getDownloadDir() {
-        if (downloadDir == null) {
-            // Return a default directory or handle this situation in a more user-friendly way
-            return System.getProperty("java.io.tmpdir");
+    private String selectFileForUploadJ2ME() {
+        try {
+            FileConnection fc = (FileConnection) Connector.open("file:///root1/");
+            if (fc.isDirectory()) {
+                Enumeration fileEnum = fc.list();
+                Vector fileVector = new Vector();
+                while (fileEnum.hasMoreElements()) {
+                    String file = (String) fileEnum.nextElement();
+                    fileVector.addElement(file);
+                }
+                // Create a file chooser to select a file
+                FileChooser fileChooser = new FileChooser();
+                fileChooser.setFiles(fileVector);
+                int selectedIndex = fileChooser.showFileChooser();
+                if (selectedIndex != -1) {
+                    String selectedFile = (String) fileVector.elementAt(selectedIndex);
+                    fc.close();
+                    return selectedFile;
+                } else {
+                    fc.close();
+                    return null;
+                }
+            } else {
+                fc.close();
+                return null;
+            }
+        } catch (Exception e) {
+            return null;
         }
-        return downloadDir;
+    }
+
+    private String selectFileForUploadJVM() {
+        JFileChooser fileChooser = new JFileChooser();
+        int returnValue = fileChooser.showOpenDialog(null);
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+            return selectedFile.getAbsolutePath();
+        } else {
+            return null;
+        }
     }
 }
