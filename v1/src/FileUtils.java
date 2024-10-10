0da1288c037cd99821cf8cad311f811399278341
transmigrator <project@transmigrator.org>
2024-10-10 14:36:01 +0200

    FileUtils.java

--- v1/src/FileUtils.java
+++ v1/src/FileUtils.java
@@ -1,150 +1,80 @@
 import javax.microedition.io.Connector;
 import javax.microedition.io.FileConnection;
-import javax.microedition.lcdui.Choice;
-import javax.microedition.lcdui.ChoiceGroup;
-import javax.microedition.lcdui.Command;
-import javax.microedition.lcdui.CommandListener;
-import javax.microedition.lcdui.Display;
-import javax.microedition.lcdui.Displayable;
-import javax.microedition.lcdui.Form;
-import javax.microedition.lcdui.List;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Enumeration;
-import java.util.Vector;
-import javax.swing.JFileChooser;
+import java.io.File;
+import java.io.IOException;
 
 public class FileUtils {
     public static String selectFile() {
+        return FileChooser.selectFile();
+    }
+
+    public static String selectFileJ2ME() {
         try {
-            // Try to use the J2ME implementation
-            return selectFileJ2ME();
-        } catch (Exception e) {
-            // Fall back to the JVM implementation
-            return selectFileJVM();
-        }
-    }
-
-    private static String selectFileJ2ME() {
-        // Create a file chooser UI component
-        FileChooser fileChooser = new FileChooser();
-        // Show the file chooser UI component
-        fileChooser.show();
-        // Return the selected file path
-        return fileChooser.getSelectedFile();
-    }
-
-    private static String selectFileJVM() {
-        // Create a file chooser
-        JFileChooser fileChooser = new JFileChooser();
-        // Show the file chooser dialog
-        int returnValue = fileChooser.showOpenDialog(null);
-        // Check if a file was selected
-        if (returnValue == JFileChooser.APPROVE_OPTION) {
-            // Return the selected file path
-            return fileChooser.getSelectedFile().getPath();
-        } else {
-            // Return null if no file was selected
-            return null;
-        }
-    }
-
-    public static class FileChooser extends Form implements CommandListener {
-        private String selectedFile;
-        private ChoiceGroup cg;
-        private Command selectCommand;
-
-        public FileChooser() {
-            super("Select File");
-            // Create a list of files
-            String[] files = getFileList();
-            cg = new ChoiceGroup("Files", Choice.POPUP);
-            for (int i = 0; i < files.length; i++) {
-                cg.append(files[i], null);
-            }
-            append(cg);
-            // Create a command to select the file
-            selectCommand = new Command("Select", Command.OK, 1);
-            addCommand(selectCommand);
-            setCommandListener(this);
-        }
-
-        public void commandAction(Command c, Displayable d) {
-            if (c == selectCommand) {
-                // Get the selected file
-                selectedFile = cg.getString(cg.getSelectedIndex());
-                // Go back to the previous screen
-                Display.getDisplay(null).setCurrent(null);
-            }
-        }
-
-        public String getSelectedFile() {
-            return selectedFile;
-        }
-
-        public void show() {
-            Display.getDisplay(null).setCurrent(this);
-        }
-
-        private String[] getFileList() {
-            // Check if we are running on JVM or J2ME
-            String platform = System.getProperty("microedition.platform");
-            if (platform != null) {
-                // Running on J2ME, use jsr-75 FileConnection API
-                return getFileListJ2ME();
-            } else {
-                // Running on JVM, use file:/// protocol
-                return getFileListJVM();
-            }
-        }
-
-        private String[] getFileListJ2ME() {
-            // Get a list of files from the device's file system
-            String[] files = null;
-            try {
-                // Get the root directory of the file system
-                FileConnection fc = (FileConnection) Connector.open("file:///root1/");
-                if (fc.isDirectory()) {
-                    // Get a list of files in the root directory
-                    Enumeration fileEnum = fc.list();
-                    Vector fileVector = new Vector();
-                    while (fileEnum.hasMoreElements()) {
-                        String file = (String) fileEnum.nextElement();
-                        if (file.endsWith(".txt")) {
-                            fileVector.addElement(file);
-                        }
-                    }
-                    files = new String[fileVector.size()];
-                    fileVector.copyInto(files);
-                }
-                fc.close();
-            } catch (Exception e) {
-                // Handle exception
-            }
-            return files;
-        }
-
-        private String[] getFileListJVM() {
-            // Get a list of files from the device's file system
-            String[] files = null;
-            try {
-                // Get the root directory of the file system
-                java.io.File rootDir = new java.io.File("file:///.");
-                // Get a list of files in the root directory
-                String[] fileArray = rootDir.list();
+            FileConnection fc = (FileConnection) Connector.open("file:///root1/");
+            if (fc.isDirectory()) {
+                Enumeration fileEnum = fc.list();
                 Vector fileVector = new Vector();
-                for (int i = 0; i < fileArray.length; i++) {
-                    String file = fileArray[i];
+                while (fileEnum.hasMoreElements()) {
+                    String file = (String) fileEnum.nextElement();
                     if (file.endsWith(".txt")) {
                         fileVector.addElement(file);
                     }
                 }
-                files = new String[fileVector.size()];
-                fileVector.copyInto(files);
-            } catch (Exception e) {
-                // Handle exception
+                String selectedFile = (String) fileVector.elementAt(0);
+                fc.close();
+                return selectedFile;
+            } else {
+                fc.close();
+                return null;
             }
-            return files;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public static String selectFileJVM() {
+        // This method is no longer needed, as FileChooser handles JVM file selection
+        return FileChooser.selectFile();
+    }
+
+    public static void saveFile(String fileName, String fileContent) {
+        if (isJ2ME()) {
+            saveFileJ2ME(fileName, fileContent);
+        } else {
+            saveFileJVM(fileName, fileContent);
+        }
+    }
+
+    private static boolean isJ2ME() {
+        return System.getProperty("microedition.configuration") != null;
+    }
+
+    private static void saveFileJ2ME(String fileName, String fileContent) {
+        try {
+            FileConnection fc = (FileConnection) Connector.open("file:///" + fileName);
+            if (!fc.exists()) {
+                fc.create();
+            }
+            OutputStream os = fc.openOutputStream();
+            os.write(fileContent.getBytes());
+            os.close();
+            fc.close();
+        } catch (Exception e) {
+            // Handle exception
+        }
+    }
+
+    private static void saveFileJVM(String fileName, String fileContent) {
+        try {
+            File file = new File(fileName);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileWriter fw = new FileWriter(file);
+            fw.write(fileContent);
+            fw.close();
+        } catch (IOException e) {
+            // Handle exception
         }
     }
 }
