c0225e6575a72736f1784ab3420a5dc0fb924302
transmigrator <project@transmigrator.org>
2024-10-10 12:56:50 +0200

    FileManager.java

--- v1/src/FileManager.java
+++ v1/src/FileManager.java
@@ -7,12 +7,7 @@ public class FileManager {
     private FileUtils fileUtils;
 
     public FileManager() {
-        String environment = System.getProperty("java.vm.name");
-        if (environment.contains("J2ME")) {
-            fileUtils = new J2MEFileUtils();
-        } else {
-            fileUtils = new JVMFileUtils();
-        }
+        fileUtils = new FileUtils();
     }
 
     public void downloadFile(String url, String filename, FileProgressListener listener) {
@@ -38,34 +33,4 @@ public class FileManager {
     public String selectFile() {
         return fileUtils.selectFile();
     }
-}
-
-// FileUtils.java
-
-public interface FileUtils {
-    void downloadFile(String url, String filename, FileProgressListener listener);
-    void uploadFile(String filename, String url);
-    List<String> listFiles();
-    void deleteFile(String filename);
-    void createDirectory(String directoryName);
-    String selectFile();
-}
-
-// J2MEFileUtils.java
-
-import javax.microedition.io.Connection;
-import javax.microedition.io.Connector;
-import javax.microedition.io.file.FileConnection;
-
-public class J2MEFileUtils implements FileUtils {
-    // Implementation of FileUtils for Java ME environment
-}
-
-// JVMFileUtils.java
-
-import java.io.File;
-import java.io.IOException;
-
-public class JVMFileUtils implements FileUtils {
-    // Implementation of FileUtils for JVM environment
 }
