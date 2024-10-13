1b382d07088a9b2f446df9681bb8524a653dda0e
transmigrator <project@transmigrator.org>
2024-10-14 00:29:25 +0200

    Delete v1/python/digital_signature.py

--- v1/python/digital_signature.py
+++
@@ -1,8 +0,0 @@
-# digital_signature.py
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric import ec
-from cryptography.hazmat.primitives import hashes
-
-def sign(data, private_key):
-    signature = private_key.sign(data, ec.ECDSA(hashes.SHA384()))
-    return signature
