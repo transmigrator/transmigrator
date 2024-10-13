4e0581c8532ebcf32fd82cc45a264515606a1d29
transmigrator <project@transmigrator.org>
2024-10-14 00:08:48 +0200

    Segmentation.java

--- v1/src/Segmentation.java
+++ v1/src/Segmentation.java
@@ -1,38 +1,49 @@
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Segmentation {
-    private int maxPacketSize;
+    private static final int MAX_PACKET_SIZE = 1280;
+    private SshuttleWrapper sshuttleWrapper;
 
-    public Segmentation(int maxPacketSize) {
-        this.maxPacketSize = maxPacketSize;
+    public Segmentation(SshuttleWrapper sshuttleWrapper) {
+        this.sshuttleWrapper = sshuttleWrapper;
     }
 
     public List<Packet> segmentData(byte[] data) {
         List<Packet> packets = new ArrayList<>();
         int offset = 0;
+        int sequenceNumber = 0;
 
         while (offset < data.length) {
-            int chunkSize = Math.min(maxPacketSize, data.length - offset);
+            int chunkSize = Math.min(MAX_PACKET_SIZE, data.length - offset);
             byte[] chunk = new byte[chunkSize];
             System.arraycopy(data, offset, chunk, 0, chunkSize);
-            Packet packet = new Packet(chunk);
+            Packet packet = new Packet(chunk, sequenceNumber, null, null, null, null, null, null);
             packets.add(packet);
             offset += chunkSize;
+            sequenceNumber++;
         }
 
         return packets;
     }
 
-    public static class Packet {
-        private byte[] data;
+    public byte[] consolidatePackets(List<Packet> packets) {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        int expectedSequenceNumber = 0;
 
-        public Packet(byte[] data) {
-            this.data = data;
+        for (Packet packet : packets) {
+            if (!sshuttleWrapper.verify(packet.getData(), packet.getDigitalSignatureAuthKeyServerOutput(), packet.getKeyExchangeKeyServerOutput())) {
+                throw new RuntimeException("Packet signature verification failed");
+            }
+            if (packet.getSequenceNumber() != expectedSequenceNumber) {
+                throw new RuntimeException("Missing or corrupted packet");
+            }
+            bos.write(packet.getData());
+            expectedSequenceNumber++;
         }
 
-        public byte[] getData() {
-            return data;
-        }
+        return bos.toByteArray();
     }
 }
