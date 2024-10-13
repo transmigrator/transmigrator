728e78485b25a20c90b328b3187e23fd59c82869
transmigrator <project@transmigrator.org>
2024-10-14 00:08:17 +0200

    Packet.java

--- v1/src/Packet.java
+++ v1/src/Packet.java
@@ -1,31 +1,53 @@
 public class Packet {
     private byte[] data;
-    private String source;
-    private String destination;
+    private int sequenceNumber;
+    private PythonObject keyExchangeKeyClientInput;
+    private PythonObject digitalSignatureAuthKeyClientInput;
+    private PythonObject encryptionKeyClientInput;
+    private PythonObject keyExchangeKeyServerOutput;
+    private PythonObject digitalSignatureAuthKeyServerOutput;
+    private PythonObject decryptionKeyServerOutput;
 
-    public Packet(byte[] data, String source, String destination) {
+    public Packet(byte[] data, int sequenceNumber, PythonObject keyExchangeKeyClientInput, PythonObject digitalSignatureAuthKeyClientInput, PythonObject encryptionKeyClientInput, PythonObject keyExchangeKeyServerOutput, PythonObject digitalSignatureAuthKeyServerOutput, PythonObject decryptionKeyServerOutput) {
         this.data = data;
-        this.source = source;
-        this.destination = destination;
+        this.sequenceNumber = sequenceNumber;
+        this.keyExchangeKeyClientInput = keyExchangeKeyClientInput;
+        this.digitalSignatureAuthKeyClientInput = digitalSignatureAuthKeyClientInput;
+        this.encryptionKeyClientInput = encryptionKeyClientInput;
+        this.keyExchangeKeyServerOutput = keyExchangeKeyServerOutput;
+        this.digitalSignatureAuthKeyServerOutput = digitalSignatureAuthKeyServerOutput;
+        this.decryptionKeyServerOutput = decryptionKeyServerOutput;
     }
 
     public byte[] getData() {
         return data;
     }
 
-    public String getSource() {
-        return source;
+    public int getSequenceNumber() {
+        return sequenceNumber;
     }
 
-    public String getDestination() {
-        return destination;
+    public PythonObject getKeyExchangeKeyClientInput() {
+        return keyExchangeKeyClientInput;
     }
 
-    public void encrypt() {
-        // ...
+    public PythonObject getDigitalSignatureAuthKeyClientInput() {
+        return digitalSignatureAuthKeyClientInput;
     }
 
-    public void decrypt() {
-        // ...
+    public PythonObject getEncryptionKeyClientInput() {
+        return encryptionKeyClientInput;
+    }
+
+    public PythonObject getKeyExchangeKeyServerOutput() {
+        return keyExchangeKeyServerOutput;
+    }
+
+    public PythonObject getDigitalSignatureAuthKeyServerOutput() {
+        return digitalSignatureAuthKeyServerOutput;
+    }
+
+    public PythonObject getDecryptionKeyServerOutput() {
+        return decryptionKeyServerOutput;
     }
 }
