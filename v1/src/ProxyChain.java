f02629d4e276e8028435df63c33327341541ce47
transmigrator <project@transmigrator.org>
2024-10-14 13:28:55 +0200

    ProxyChain.java

--- v1/src/ProxyChain.java
+++ v1/src/ProxyChain.java
@@ -1,4 +1,12 @@
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.jsse.BCSSLContext;
+import org.bouncycastle.jsse.BCSSLParameters;
+import org.bouncycastle.jsse.BCSSLSocketFactory;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
 import java.net.InetSocketAddress;
+import java.security.Security;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,12 +16,15 @@ import java.util.List;
 public class ProxyChain {
     private List<InetSocketAddress> proxies;
     private static final int MAX_PROXIES = 3;
+    private SSLSocketFactory sslSocketFactory;
 
     /**
      * Constructs a new ProxyChain object.
      */
     public ProxyChain() {
         this.proxies = new ArrayList<>(MAX_PROXIES);
+        // Initialize Bouncy Castle provider
+        Security.addProvider(new BouncyCastleProvider());
     }
 
     /**
@@ -39,5 +50,35 @@ public class ProxyChain {
      */
     public List<InetSocketAddress> getProxies() {
         return proxies;
+    }
+
+    /**
+     * Sets the SSL socket factory for the proxy chain.
+     *
+     * @param sslSocketFactory the SSL socket factory
+     */
+    public void setSslSocketFactory(SSLSocketFactory sslSocketFactory) {
+        this.sslSocketFactory = sslSocketFactory;
+    }
+
+    /**
+     * Creates a default SSL socket factory using Bouncy Castle.
+     *
+     * @return the default SSL socket factory
+     */
+    public SSLSocketFactory createDefaultSslSocketFactory() {
+        try {
+            // Create a Bouncy Castle SSL context
+            SSLContext sslContext = BCSSLContext.getInstance("TLS", new BouncyCastleProvider());
+            // Create a Bouncy Castle SSL socket factory
+            BCSSLSocketFactory sslSocketFactory = (BCSSLSocketFactory) sslContext.getSocketFactory();
+            // Set the SSL parameters
+            BCSSLParameters sslParameters = new BCSSLParameters();
+            // ... customize SSL parameters as needed ...
+            sslSocketFactory.setParameters(sslParameters);
+            return sslSocketFactory;
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create default SSL socket factory", e);
+        }
     }
 }
