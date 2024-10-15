db6f9d41d458ffd2be8ecb2f8518706e4041f4c3
transmigrator <project@transmigrator.org>
2024-10-15 20:28:38 +0200

    mod.rs

--- v1/src/browser/mod.rs
+++ v1/src/browser/mod.rs
@@ -1,47 +1,6 @@
 // browser/mod.rs
 
-use wasm_bindgen::prelude::*;
-use web_sys::window;
+mod interface;
+mod tab;
 
-#[wasm_bindgen]
-pub struct Browser {
-    address_bar: String,
-}
-
-#[wasm_bindgen]
-impl Browser {
-    pub fn new() -> Self {
-        Self {
-            address_bar: String::new(),
-        }
-    }
-
-    pub fn set_address(&mut self, address: String) {
-        self.address_bar = address;
-    }
-
-    pub fn navigate(&self) -> Result<(), JsValue> {
-        let window = window().expect("no global `window` exists");
-        let document = window.document().expect("should have a document on window");
-        let body = document.body().expect("document should have a body");
-
-        // For now, just display the address in the body
-        body.set_inner_html(&format!("Navigating to: {}", self.address_bar));
-
-        Ok(())
-    }
-
-    pub fn detect_command(&self) -> bool {
-        self.address_bar.starts_with('/')
-    }
-
-    pub fn execute_command(&self) -> Result<(), JsValue> {
-        if self.detect_command() {
-            let window = window().expect("no global `window` exists");
-            let document = window.document().expect("should have a document on window");
-            let body = document.body().expect("document should have a body");
-            body.set_inner_html(&format!("Executing command: {}", self.address_bar));
-        }
-        Ok(())
-    }
-}
+pub use interface::BrowserInterface;
