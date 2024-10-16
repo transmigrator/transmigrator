ebf35c8c9b619b37db36a6927610f4d60dd6c8f6
transmigrator <project@transmigrator.org>
2024-10-16 20:54:51 +0200

    mod.rs

--- v1/src/file_manager/mod.rs
+++ v1/src/file_manager/mod.rs
@@ -1,19 +1,10 @@
 // file_manager/mod.rs
 
-pub fn window() -> Option<web_sys::Window> {
-    web_sys::window()
-}
-use web_sys::HtmlInputElement;
-use web_sys::ProgressEvent;
-use js_sys::Array;
-use web_sys::FileReader;
-use web_sys::Blob;
-use web_sys::Url;
 use std::fs::File;
 use std::io::{Read, Write};
 
 pub struct FileManager {
-    selected_file: Option<HtmlInputElement>,
+    selected_file: Option<String>,
 }
 
 impl FileManager {
@@ -23,53 +14,27 @@ impl FileManager {
         }
     }
 
-    pub fn select_file(&mut self) -> Result<(), JsValue> {
-        let document = window().unwrap().unwrap().document().unwrap();
-        let input = document.create_element("input")?.dyn_into::<HtmlInputElement>()?;
-        input.set_attribute("type", "file")?;
-        input.set_attribute("style", "display: none;")?;
-        document.body().unwrap().append_child(&input)?;
-        input.click();
-        self.selected_file = Some(input);
+    pub fn select_file(&mut self, file_path: &str) {
+        self.selected_file = Some(file_path.to_string());
+    }
+
+    pub fn download_file(&self, data: &str, filename: &str) -> Result<(), std::io::Error> {
+        let mut file = File::create(filename)?;
+        file.write_all(data.as_bytes())?;
         Ok(())
     }
 
-    pub fn download_file(&self, data: &str, filename: &str) -> Result<(), JsValue> {
-        let document = window().ok_or("no global `window` exists")?.document().ok_or(JsValue::from_str("should have a document on window"))?;
-        let a = document.create_element("a")?;
-        let blob = Blob::new_with_str_sequence(&Array::of1(&JsValue::from_str(data)))?;
-        let url = Url::create_object_url_with_blob(&blob)?;
-        a.set_attribute("href", &url)?;
-        a.set_attribute("download", filename)?;
-        a.set_attribute("style", "display: none;")?;
-        document.body().unwrap().append_child(&a)?;
-        a.click();
-        Url::revoke_object_url(&url)?;
-        Ok(())
-    }
-
-    pub fn upload_file(&self) -> Result<(), JsValue> {
-        if let Some(input) = &self.selected_file {
-            let files = input.files().unwrap();
-            if files.length() > 0 {
-                let file = files.get(0).unwrap();
-                let reader = FileReader::new()?;
-                let onloadend = Closure::wrap(Box::new(move |event: web_sys::ProgressEvent| {
-                    let reader = event.target().unwrap().dyn_into::<FileReader>().unwrap();
-                    let result = reader.result().unwrap();
-                    let text = result.as_string().unwrap();
-                    web_sys::console::log_1(&text.into());
-                }) as Box<dyn FnMut(_)>);
-                reader.set_onloadend(Some(onloadend.as_ref().unchecked_ref()));
-                reader.read_as_text(&file)?;
-                onloadend.forget();
-            }
+    pub fn upload_file(&self) -> Result<String, std::io::Error> {
+        if let Some(file_path) = &self.selected_file {
+            let mut file = File::open(file_path)?;
+            let mut contents = String::new();
+            file.read_to_string(&mut contents)?;
+            Ok(contents)
+        } else {
+            Err(std::io::Error::new(std::io::ErrorKind::NotFound, "No file selected"))
         }
-        Ok(())
     }
-}
 
-impl FileManager {
     pub fn read_file(path: &str) -> Result<Vec<u8>, std::io::Error> {
         let mut file = File::open(path)?;
         let mut buffer = Vec::new();
