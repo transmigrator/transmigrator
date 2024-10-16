c87522ced415fd98cdb0dffccc9a401b38ae742a
transmigrator <project@transmigrator.org>
2024-10-16 15:11:12 +0200

    mod.rs

--- v1/src/crypto/mod.rs
+++ v1/src/crypto/mod.rs
@@ -1,6 +1,6 @@
 // src/crypto/mod.rs
 
-use ring::aead::{self, Aad, BoundKey, LessSafeKey, Nonce, UnboundKey, AES_256_GCM};
+use ring::aead::{Aad, LessSafeKey, Nonce, UnboundKey, AES_256_GCM};
 use ring::rand::{SecureRandom, SystemRandom};
 
 const NONCE_SIZE: usize = 12;
@@ -9,13 +9,13 @@ pub fn encrypt_packet(packet: &[u8], key: &[u8]) -> Vec<u8> {
     let unbound_key = UnboundKey::new(&AES_256_GCM, key).expect("Invalid key length");
     let nonce = generate_nonce();
     let mut in_out = packet.to_vec();
+    let nonce_clone = nonce.clone();
     let nonce = Nonce::assume_unique_for_key(nonce);
     let key = LessSafeKey::new(unbound_key);
 
     key.seal_in_place_append_tag(nonce, Aad::empty(), &mut in_out)
         .expect("Encryption failed");
-
-    [nonce.as_ref(), in_out.as_slice()].concat()
+    [nonce_clone.as_ref(), in_out.as_slice()].concat()
 }
 
 pub fn decrypt_packet(encrypted_packet: &[u8], key: &[u8]) -> Vec<u8> {
