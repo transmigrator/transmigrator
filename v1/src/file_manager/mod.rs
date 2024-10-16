dc849a70fa38d83f35ec772c6b9c3b73632d6065
transmigrator <project@transmigrator.org>
2024-10-16 16:32:39 +0200

    mod.rs

--- v1/src/file_manager/mod.rs
+++ v1/src/file_manager/mod.rs
@@ -1,7 +1,15 @@
 // file_manager/mod.rs
 
 use wasm_bindgen::prelude::*;
-use web_sys::{window, HtmlInputElement, FileReader, Blob, Url};
+pub fn window() -> Option<web_sys::Window> {
+    web_sys::window()
+}
+use web_sys::HtmlInputElement;
+use web_sys::ProgressEvent;
+use js_sys::Array;
+use web_sys::FileReader;
+use web_sys::Blob;
+use web_sys::Url;
 
 #[wasm_bindgen]
 pub struct FileManager {
@@ -18,7 +26,7 @@ impl FileManager {
     }
 
     pub fn select_file(&mut self) -> Result<(), JsValue> {
-        let document = window().unwrap().document().unwrap();
+        let document = window().unwrap().unwrap().document().unwrap();
         let input = document.create_element("input")?.dyn_into::<HtmlInputElement>()?;
         input.set_attribute("type", "file")?;
         input.set_attribute("style", "display: none;")?;
@@ -29,7 +37,7 @@ impl FileManager {
     }
 
     pub fn download_file(&self, data: &str, filename: &str) -> Result<(), JsValue> {
-        let document = window().unwrap().document().unwrap();
+        let document = window().ok_or("no global `window` exists")?.document().ok_or(JsValue::from_str("should have a document on window"))?;
         let a = document.create_element("a")?;
         let blob = Blob::new_with_str_sequence(&Array::of1(&JsValue::from_str(data)))?;
         let url = Url::create_object_url_with_blob(&blob)?;
