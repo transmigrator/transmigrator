0e92d5e7006a4c1e280b909dd1d2c5c0524a42a0
transmigrator <project@transmigrator.org>
2024-10-17 12:20:37 +0200

    utils.rs

--- v1/src/utils.rs
+++ v1/src/utils.rs
@@ -1,7 +1,25 @@
 use reqwest::Error;
+use std::sync::Mutex;
+use lazy_static::lazy_static;
 
-pub async fn fetch_proxies(url: &str) -> Result<Vec<String>, Error> {
+lazy_static! {
+    static ref PROXIES: Mutex<Vec<String>> = Mutex::new(Vec::new());
+}
+
+pub async fn fetch_proxies(url: &str) -> Result<(), Error> {
     let response = reqwest::get(url).await?;
     let proxies = response.text().await?;
-    Ok(proxies.lines().map(|line| line.to_string()).collect())
+    let mut proxies_vec = PROXIES.lock().unwrap();
+    *proxies_vec = proxies.lines().map(|line| line.to_string()).collect();
+    Ok(())
+}
+
+pub fn get_proxies() -> Vec<String> {
+    let proxies_vec = PROXIES.lock().unwrap();
+    proxies_vec.clone()
+}
+
+pub fn clear_proxies() {
+    let mut proxies_vec = PROXIES.lock().unwrap();
+    proxies_vec.clear();
 }
