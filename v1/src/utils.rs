cf1732dec8b96b2cb3ad409d5a39f3a407c9dff0
transmigrator <project@transmigrator.org>
2024-10-17 21:18:04 +0200

    utils.rs

--- v1/src/utils.rs
+++ v1/src/utils.rs
@@ -1,20 +1,42 @@
 use reqwest::Error;
 use std::sync::Mutex;
 use lazy_static::lazy_static;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsValue;
+use js_sys::Function;
+use wasm_bindgen_futures::spawn_local;
 
 lazy_static! {
     static ref PROXIES: Mutex<Vec<String>> = Mutex::new(Vec::new());
 }
 
-pub async fn fetch_proxies(url: &str) -> Result<(), Error> {
+pub async fn fetch_proxies_util(url: &str) -> Result<(), Error> {
     let response = reqwest::get(url).await?;
-    if !response.status().is_success() {
+    if (!response.status().is_success()) {
         return Err(Error::new(reqwest::StatusCode::BAD_REQUEST, "Failed to fetch proxies"));
     }
     let proxies = response.text().await?;
     let mut proxies_vec = PROXIES.lock().unwrap();
     *proxies_vec = proxies.lines().map(|line| line.to_string()).collect();
     Ok(())
+}
+
+#[wasm_bindgen]
+pub fn fetch_proxies(url: &str, callback: Function) {
+    let url = url.to_string();
+    spawn_local(async move {
+        match fetch_proxies_util(&url).await {
+            Ok(_) => {
+                log::info!("Fetched proxies successfully");
+                let proxies = get_proxies();
+                let js_proxies = JsValue::from_serde(&proxies).unwrap();
+                callback.call1(&JsValue::NULL, &js_proxies).unwrap();
+            }
+            Err(err) => {
+                log::error!("Failed to fetch proxies: {:?}", err);
+            }
+        }
+    });
 }
 
 pub fn get_proxies() -> Vec<String> {
