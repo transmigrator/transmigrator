b79db451d7b984ca7149b5a3a62ac870900aff6c
transmigrator <project@transmigrator.org>
2024-10-17 12:39:33 +0200

    dns.rs

--- v1/src/dns.rs
+++ v1/src/dns.rs
@@ -3,8 +3,8 @@ use serde_json::Value;
 
 pub async fn resolve_dns(domain: &str) -> Result<String, Error> {
     let client = Client::new();
-    let url = format!("https://dns.google/resolve?name={}", domain);
-    let response = client.get(&url).send().await?.json::<Value>().await?;
+    let url = format!("https://cloudflare-dns.com/dns-query?name={}", domain);
+    let response = client.get(&url).header("Accept", "application/dns-json").send().await?.json::<Value>().await?;
     let ip = response["Answer"][0]["data"].as_str().unwrap().to_string();
     Ok(ip)
 }
