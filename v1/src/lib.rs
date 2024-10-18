afd5cd82e06af6d8bbbb8a76285b4948e12647c4
transmigrator <project@transmigrator.org>
2024-10-18 12:41:45 +0200

    lib.rs

--- v1/src/lib.rs
+++ v1/src/lib.rs
@@ -26,7 +26,7 @@ pub fn main() -> Result<(), JsValue> {
 pub fn fetch_proxies(url: &str, callback: Function) {
     let url = url.to_string();
     spawn_local(async move {
-        match fetch_proxies_util(&url).await {
+        match fetch_proxies_util(&url, callback).await {
             Ok(_) => {
                 log::info!("Fetched proxies successfully");
                 let mut proxy_mesh = ProxyMesh::new();
