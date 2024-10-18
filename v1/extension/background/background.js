2409014cab3f276825c1e8ae8ca32f85cc2c2def
transmigrator <project@transmigrator.org>
2024-10-18 11:08:37 +0200

    background.js

--- v1/extension/background/background.js
+++ v1/extension/background/background.js
@@ -1,9 +1,11 @@
 import init, { handle_fetch } from './pkg/transmigrator.js';
 
+// Register the fetch event listener to call the handle_fetch function from Rust
 self.addEventListener('fetch', event => {
     handle_fetch(event);
 });
 
+// Initialize the WebAssembly module
 init().then(() => {
     console.log('Wasm module initialized');
 });
