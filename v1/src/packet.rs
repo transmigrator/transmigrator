05af2cb98049ebed5416c6964bd16e9f3c081a4e
transmigrator <project@transmigrator.org>
2024-10-19 20:30:36 +0200

    packet.rs

--- v1/src/packet.rs
+++ v1/src/packet.rs
@@ -59,3 +59,69 @@ impl Packet {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_packet_creation() {
+        let data = vec![1, 2, 3];
+        let key = vec![0; 32];
+        let packet = Packet::new(data.clone(), key.clone());
+
+        assert_eq!(packet.get_data(), &data);
+        assert_eq!(packet.get_key(), &key);
+        assert_eq!(packet.get_iv().len(), 16);
+    }
+
+    #[test]
+    fn test_packet_encryption_decryption() {
+        let data = vec![1, 2, 3];
+        let key = vec![0; 32];
+        let mut packet = Packet::new(data.clone(), key);
+
+        packet.encrypt().unwrap();
+        assert_ne!(packet.get_data(), &data);
+
+        packet.decrypt().unwrap();
+        assert_eq!(packet.get_data(), &data);
+    }
+
+    #[test]
+    fn test_packet_set_get_data() {
+        let mut packet = Packet::new(vec![], vec![0; 32]);
+        let data = vec![1, 2, 3];
+        packet.set_data(data.clone());
+
+        assert_eq!(packet.get_data(), &data);
+    }
+
+    #[test]
+    fn test_packet_set_get_key() {
+        let mut packet = Packet::new(vec![], vec![0; 32]);
+        let key = vec![1; 32];
+        packet.set_key(key.clone());
+
+        assert_eq!(packet.get_key(), &key);
+    }
+
+    #[test]
+    fn test_packet_set_get_iv() {
+        let mut packet = Packet::new(vec![], vec![0; 32]);
+        let iv = vec![1; 16];
+        packet.set_iv(iv.clone());
+
+        assert_eq!(packet.get_iv(), &iv);
+    }
+
+    #[test]
+    fn test_packet_ensure_size() {
+        let mut packet = Packet::new(vec![1, 2, 3], vec![0; 32]);
+        packet.ensure_size();
+
+        assert_eq!(packet.get_data().len(), 1280);
+        assert_eq!(packet.get_data()[0..3], [1, 2, 3]);
+        assert_eq!(packet.get_data()[3..], vec![0; 1277]);
+    }
+}
