156e01ad3803a9b14404a985e01cf771ddbd4863
transmigrator <project@transmigrator.org>
2024-10-21 20:34:26 +0200

    Delete v1 directory

--- v1/src/content.rs
+++
@@ -1,8 +0,0 @@
-use wasm_bindgen::prelude::*;
-use web_sys::console;
-
-#[wasm_bindgen(start)]
-pub fn start() -> Result<(), JsValue> {
-    console::log_1(&"Content script loaded".into());
-    Ok(())
-}
